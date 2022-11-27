ec254a63a70ef13c650f7cf4c53c98f139aa11b1
TaanesC <109472397+TaanesC@users.noreply.github.com>
2022-11-27 18:25:26 +0800

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,8 @@
+	<section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By Taanes</p>
+        </div>
+    </section>
+  
+</body>
+</html>
---
+++ partials-front/menu.php
@@ -0,0 +1,80 @@
+<?php include('config/constants.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>V EVENT</title>
+
+    <link rel="stylesheet" href="cs/style.css">
+</head>
+
+<body>
+   
+
+         <div class="navbar">
+		 <style>
+		 .navbar {
+			 
+			 overflow: hidden;
+			 background-color: #333;
+			 position: sticky; 
+			 position: -webkit-sticky;
+			 top: 0;
+			 font-family: Ariel;
+		 }
+		 
+		 .navbar a { 
+		 float:left; 
+		 display: block;
+		 color: #ff0066;
+		 text-align: center;
+		 padding: 20px 40px;
+		 text-decoration: none;
+		 }
+		 .navbar a.right {
+			 float:right;
+		 }
+		 .navbar a:hover { 
+		 background-color: #ddd;
+		 color: #00cc66;
+		 }
+		 .navbar a.active { 
+		 background-color: #666;
+		 color: #ccff33;
+		 }
+                 .categories .home-slider .swiper-slide{
+    overflow: hidden;
+    border-radius: .5rem;
+    height: 5rem;
+    width:35rem;
+}
+
+.categories.home-slider .swiper-slide img{
+    height:100%;
+    width:100%;
+    object-fit: cover;
+}
+		 </style>
+		 
+		 <a href="index.php"> Home</a>
+		 		 <a  href="categories.php">Categories</a>
+				 		 		 <a  href="Event.php">Booking</a>
+								                         <a href="home.php">User</a>
+
+
+
+		 
+		 
+		 </div>
+		 
+		 <div class="about-section">
+		 <h1>V EVENT</h1>
+		 <h3>Online Event Planner</h3>
+		 </div>
+	</body>
+	</html>
+		  
+            
